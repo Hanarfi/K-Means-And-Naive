97423c761d950345041af7325242b840bc3ab4f6
Hanarfi <reihanarfi2017@gmail.com>
2026-06-23 21:39:08 +0700

    Update koneksi.py

--- database/koneksi.py
+++ database/koneksi.py
@@ -11,10 +11,18 @@ DB_PATH = BASE_DIR / "database.db"
 def get_connection():
     """
     Membuat koneksi ke database SQLite
+    dan mengaktifkan Foreign Key
     """
+
     try:
         conn = sqlite3.connect(DB_PATH)
+
+        # Mengembalikan hasil query dalam bentuk dictionary
         conn.row_factory = sqlite3.Row
+
+        # Mengaktifkan Foreign Key SQLite
+        conn.execute("PRAGMA foreign_keys = ON")
+
         return conn
 
     except sqlite3.Error as e:
