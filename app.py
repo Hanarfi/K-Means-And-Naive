d2499bb29a3afce72203db1a454297e348aa4040
Hanarfi <reihanarfi2017@gmail.com>
2026-06-24 15:00:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,16 +3,26 @@
 import streamlit as st
 
 from pages.login import show as login_page
+from pages.register import show as register_page
 from pages.dashboard import show as dashboard_page
 
 
-# Inisialisasi session
 if "login" not in st.session_state:
     st.session_state["login"] = False
 
+if "halaman" not in st.session_state:
+    st.session_state["halaman"] = "login"
 
-# Routing sederhana
+
+# Sudah login
 if st.session_state["login"]:
     dashboard_page()
+
+# Belum login
 else:
-    login_page()
+
+    if st.session_state["halaman"] == "login":
+        login_page()
+
+    elif st.session_state["halaman"] == "register":
+        register_page()
