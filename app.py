2061255fc53c459045314e9461406ade899ebf08
Hanarfi <reihanarfi2017@gmail.com>
2026-06-24 14:59:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
-from pages.register import show
+# app.py
 
-show()
+import streamlit as st
+
+from pages.login import show as login_page
+from pages.dashboard import show as dashboard_page
+
+
+# Inisialisasi session
+if "login" not in st.session_state:
+    st.session_state["login"] = False
+
+
+# Routing sederhana
+if st.session_state["login"]:
+    dashboard_page()
+else:
+    login_page()
