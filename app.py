bb57d748551bd70d3755eb7aabf132b47eaf658c
Hanarfi <reihanarfi2017@gmail.com>
2026-06-26 09:52:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit as st
 from pages.login import show as login_page
 from pages.register import show as register_page
 from pages.dashboard import show as dashboard_page
+from pages.dataset import show as dataset_page
 from assets.load_css import load_css
 st.markdown(
     load_css(),
