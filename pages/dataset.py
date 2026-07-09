3f1a4c5de7240cd4ceda6f66256dcebd46a8be15
Hanarfi <reihanarfi2017@gmail.com>
2026-07-09 17:10:51 +0700

    Update dataset.py

--- pages/dataset.py
+++ pages/dataset.py
@@ -323,36 +323,7 @@ def tampil_preview_dataset(id_dataset):
     )
 
 
-# ==========================================
-# BACA FILE EXCEL
-# ==========================================
 
-def baca_excel(file):
-
-    try:
-
-        df = pd.read_excel(file)
-
-        return True, df
-
-    except Exception as e:
-
-        return False, str(e)
-
-
-# ==========================================
-# VALIDASI DATASET
-# ==========================================
-
-def validasi_dataset(df):
-
-    kolom_error = validasi_kolom(df)
-
-    if len(kolom_error) > 0:
-
-        return False, kolom_error
-
-    return True, None
 
 
 
